75525c52c4666bcdd61c85d7498b99f6b56708f2
M1chlCZ <m1chlcz18@gmail.com>
2026-08-27 10:34:54 +0200

    Pin the verified wallet revision

--- tests/repository-config.test.sh
+++ tests/repository-config.test.sh
@@ -26,7 +26,7 @@ require_file Makefile
 
 require_line .env.example 'NETWORK=testnet'
 require_line .env.example 'NODE_RELEASE=v1.1.2.0-testnet'
-require_line .env.example 'WALLET_REF=774c9ac61ef4ac3c902dd034e366dcd6a5148fc3'
+require_line .env.example 'WALLET_REF=ecaeb6c093a584429c94360fc69f498753d4dbe5'
 require_line .env.example 'RPC_BRIDGE_REF=96074907643ae25efd043cc02ed28fd407c27882'
 require_line .env.example 'BLOCKBOOK_REF=bfeb91020e9f900e316aaade639d3f021280e055'
 
